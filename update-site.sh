2fd2dede24eaa5e312bcb7d214bdff8cede8e1a0
Ionut Cotoi <icotoi@gmail.com>
2014-10-18 15:30:39 +0300

    update-site.sh: go to base dir before updating github.io

--- update-site.sh
+++ update-site.sh
@@ -1,10 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+echo $DIR
+cd $DIR
+
 cp -r dist /tmp
 git commit -am "updated website files"
 git checkout gh-pages
 cp -r /tmp/dist/* .
 git add .
 git commit -am "updated website files"
-git push 
-git checkout master 
-git push 
+git push
+git checkout master
+git push
