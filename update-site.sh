6ec5613afab9786e93efa7f6175eca10936d5af9
Teodor Szente <teodor98sz@gmail.com>
2014-10-18 15:44:28 +0300

    updated website files

--- update-site.sh
+++ update-site.sh
@@ -5,13 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 cd $DIR
 
-#cp -r dist /tmp
-#git add website
-#git commit -am "updated website files"
-#git checkout gh-pages
-#cp -r /tmp/dist/* .
-#git add .
-#git commit -am "updated website files"
-#git push
-#git checkout master
-#git push
+cp -r dist /tmp
+git add website
+git commit -am "updated website files"
+git checkout gh-pages
+cp -r /tmp/dist/* .
+git add .
+git commit -am "updated website files"
+git push
+git checkout master
+git push
