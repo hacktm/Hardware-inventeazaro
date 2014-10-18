2034dce2fc1f4a12f4ba6c918710e2778afa378a
Ionut Cotoi <icotoi@gmail.com>
2014-10-18 14:01:42 +0300

    Added model fields based on medical form data

--- backend/medical/models.py
+++ backend/medical/models.py
@@ -5,17 +5,30 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 
-class FamilyDoctor(models.Model):
+SEX = (
+    (1, "Male"),
+    (2, "Female"),
+)
+
+
+class Doctor(models.Model):
     name = models.CharField(max_length=60)
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     age = models.IntegerField(max_length=3)
+    sex = models.IntegerField(choices=SEX)
+    cnp = models.IntegerField(max_length=13)  # CNP
     height = models.IntegerField(max_length=3)  # cm
     weight = models.IntegerField(max_length=3)  # kg
-    family_doctor = models.ForeignKey(FamilyDoctor)
+    family_doctor = models.ForeignKey(Doctor)
     blood_group = models.CharField(max_length=3)
+    phone = models.CharField(max_length=20)
+
+    emergency_contact = models.CharField(max_length=64)
+    emergency_relation = models.CharField(max_length=64)
+    emergency_phone = models.CharField(max_length=20)
 
     def __unicode__(self):
         return self.user.username
