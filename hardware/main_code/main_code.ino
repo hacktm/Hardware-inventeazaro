14f540a3661b4c04c51013223690ca85f4afe59a
Constantin Craciun <madaerodog@gmail.com>
2014-10-19 12:15:09 +0300

    replaced temperature sensor, updated main code

--- hardware/main_code/main_code.ino
+++ hardware/main_code/main_code.ino
@@ -4,9 +4,29 @@
 #include "Wire.h"
 #include "LiquidCrystal.h"
 
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+
+#define DHTPIN 8     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
 //DHT11 - temperature & humidity ambient
-dht11 DHT11;
-#define DHT11PIN 8
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
 #define temp_amb    "Temp_amb"
 #define humidity_amb    "Humidity_amb"
 #define pulse_user    "Pulse"
@@ -62,6 +82,8 @@ void setup(){
   // UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
   // AND APPLY THAT VOLTAGE TO THE A-REF PIN
   //analogReference(EXTERNAL);
+ 
+  dht.begin();
 }
 
 void loop() {
@@ -111,14 +133,35 @@ void loop() {
 
 // this method makes a HTTP connection to the devicehub server:
 void sendData() {
-   int chk = DHT11.read(DHT11PIN);
-   float sensor1 = (int)DHT11.temperature;
-   float sensor2 = (int)DHT11.humidity;
+   //int chk = DHT11.read(DHT11PIN);
+   float sensor1 = dht.readTemperature();
+   float sensor2 = dht.readHumidity();
    float sensor3 = last_bpm;
    float sensor4 = analogRead(A2); //alcool
    float sensor5 = sugar_level;
    float sensor6 = temp_body;
    float sensor7 = panic_btn;
+   
+
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();  
+   
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht.computeHeatIndex(f, h);
    
   //lcd.setCursor(0, 1);
   //lcd.print(sensor1);
