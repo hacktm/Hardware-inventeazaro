5c5e5307c6fda0740b6b5159fc953858f2f06eb1
Ionut Cotoi <icotoi@gmail.com>
2014-10-19 12:37:15 +0300

    Add ml_test

--- backend/medical/devicehub.py
+++ backend/medical/devicehub.py
@@ -1,4 +1,16 @@
 __author__ = 'cotty'
 from models import DeviceHubProject
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+
+def ml_test():
+    learn_pulse = np.array([73, 80, 84, 87, 81, 74, 80, 87, 92, 90, 75, 90, 94, 87, 84])
+    rfc = RandomForestClassifier()
+    rfc.fit_transform(np.array([learn_pulse, [1]]))
+    rfc.predict(np.array([75], [1]))
+    
+
+
 
 
