a95787e9b0beba9221339de3d8b4c27faa8863f1
Ionut Cotoi <icotoi@gmail.com>
2014-10-19 17:25:40 +0300

    set JSON as default parser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,6 +61,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
     )
 }
 
